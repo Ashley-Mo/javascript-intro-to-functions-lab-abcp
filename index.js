2f49512fb0403da83ad45225cc2b4261afb45f2c
Aswathylal Mohanlal <lal.aswathy@gmail.com>
2018-01-14 17:35:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string.toEqual(string.toLowerCase())){
+  if(string === string.toLowerCase()){
     console.log("I can't hear you!")
   }
-  else if(string.toEqual(string.toUpperCase())){
+  else if(string === string.toUpperCase()){
 console.log("YES INDEED!")
   }
-  else if(string.toEqual(string)){
+  else if(string === (string)){
     return("I love you, too.")
     
   }
