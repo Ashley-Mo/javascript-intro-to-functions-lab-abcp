6010ec8529a79828ac7dd130c408e5d7669df9a6
Aswathylal Mohanlal <lal.aswathy@gmail.com>
2018-01-14 17:28:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+ console.log(string.toUpperCase)
+  
+}
+function logWhisper(string){
+  console.log(string.toLowerCase)
+}
+
+function sayHiToGrandma(string){
+  if(string.toEqual('hello')){
+    console.log("I can't hear you!")
+  }
+  else if(string.toEqual('HELLO')){
+console.log("YES INDEED!")
+  }
+  else if(string.toEqual("I love you, Grandma.")){
+    return("I love you, too.")
+    
+  }
+  
+}
